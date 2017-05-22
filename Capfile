dddb599473a27ab228fcf41fa9a45aabdc66359f
mbv <terehovk@ya.ru>
2017-05-22 20:34:48 +0300

    WIP: Deploy

--- Capfile
+++ Capfile
@@ -38,7 +38,7 @@ require 'capistrano/rails'
 require 'capistrano/passenger'
 
 # If you are using rbenv add these lines:
-# require 'capistrano/rbenv'
+require 'capistrano/rbenv'
 set :rbenv_type, :user
 set :rbenv_ruby, '2.4.0'
 
--- config/deploy.rb
+++ config/deploy.rb
@@ -4,7 +4,7 @@ lock "3.8.1"
 set :application, "mbvgram"
 set :repo_url, "git@github.com:mbv/MbvGram.git"
 
-set :deploy_to, '/home/deploy/my_app_name'
+set :deploy_to, '/home/deploy/mbvgram'
 
 append :linked_files, "config/database.yml", "config/secrets.yml"
 append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "vendor/bundle", "public/system", "public/uploads"
