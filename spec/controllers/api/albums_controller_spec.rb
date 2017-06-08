fc37821359bd0678677981a2936e79efa98fc52c
Konstantin Terekhov <KonstantinTerekhov@coherentsolutions.com>
2017-06-08 12:57:25 +0300

    Implement albums tests

--- spec/controllers/api/albums_controller_spec.rb
+++ spec/controllers/api/albums_controller_spec.rb
@@ -3,6 +3,50 @@
 require "rails_helper"
 
 RSpec.describe Api::AlbumsController, type: :controller do
+  describe "#user_albums" do
+    let(:user) { create(:user) }
+    let(:album) { create(:album, user: user) }
+
+    context "user signed in" do
+      let(:another_user) { create(:user) }
+      let(:following_user) do
+        tmp_user = create(:user)
+        user.following << tmp_user
+        tmp_user
+      end
+      let(:album) { create(:album, user: user) }
+
+      before do
+        login_user_with_token user
+      end
+
+      it "show own albums" do
+        get :user_albums, params: { id: user.id }, format: :json
+
+        is_expected.to respond_with(200)
+      end
+
+      it "show following user albums" do
+        get :user_albums, params: { id: following_user.id }, format: :json
+
+        is_expected.to respond_with(200)
+      end
+
+      it "show albums not following user" do
+        expect do
+          get :user_albums, params: { id: another_user.id }, format: :json
+        end.to raise_error(CanCan::AccessDenied)
+      end
+    end
+    context "user not signed in" do
+      it "need authorization" do
+        get :user_albums, params: { id: user.id }, format: :json
+
+        is_expected.to respond_with(401)
+      end
+    end
+
+  end
   describe "#show" do
     context "user signed in" do
       let(:user) { create(:user) }
@@ -18,20 +62,20 @@ RSpec.describe Api::AlbumsController, type: :controller do
         login_user_with_token user
       end
 
-      it "own album" do
+      it "show own album" do
         get :show, params: { id: album }, format: :json
         is_expected.to respond_with(200)
         expect(assigns(:_resource)).to eq album
       end
 
-      it "album following user" do
+      it "show album following user" do
         album.update_attributes(user: following_user)
         get :show, params: { id: album }, format: :json
         is_expected.to respond_with(200)
         expect(assigns(:_resource)).to eq album
       end
 
-      it "album not following user" do
+      it "show album not following user" do
         album.update_attributes(user: another_user)
         expect do
           get :show, params: { id: album }, format: :json
@@ -50,13 +94,13 @@ RSpec.describe Api::AlbumsController, type: :controller do
 
   describe "#create" do
     context "user signed in" do
-      let(:user) { create(:user, id: 150) }
+      let(:user) { create(:user) }
 
       before do
         login_user_with_token user
       end
 
-      it "new album" do
+      it "create album" do
         album = { title:       "album title",
                   description: "album description",
                   tag_list:    [] }
@@ -72,6 +116,86 @@ RSpec.describe Api::AlbumsController, type: :controller do
                   description: "album description",
                   tag_list:    [] }
         post :create, body: album.to_json, format: :json
+
+        is_expected.to respond_with(401)
+      end
+    end
+  end
+
+  describe "#update" do
+    context "user signed in" do
+      let(:user) { create(:user) }
+      let(:another_user) { create(:user) }
+      let(:album) { create(:album, user: user) }
+
+      before do
+        login_user_with_token user
+      end
+
+      it "update album" do
+        album_hash = { title:       "album title",
+                       description: "album description",
+                       tag_list:    [] }
+        put :update, params: { id: album.id }, body: album_hash.to_json, as: :json, format: :json
+
+        expect(assigns(:_resource)).to have_attributes(title:       album_hash[:title],
+                                                       description: album_hash[:description])
+      end
+      it "can't update another's album" do
+        album.update_attributes(user: another_user)
+        album_hash = { title:       "album title",
+                       description: "album description",
+                       tag_list:    [] }
+        expect do
+          put :update, params: { id: album.id }, body: album_hash.to_json, as: :json, format: :json
+        end.to raise_error(CanCan::AccessDenied)
+      end
+
+
+    end
+    context "user not signed in" do
+      let(:album) { create(:album) }
+
+      it "need authorization" do
+        album_hash = { title:       "album title",
+                       description: "album description",
+                       tag_list:    [] }
+        put :update, params: { id: album.id }, body: album_hash.to_json, format: :json
+
+        is_expected.to respond_with(401)
+      end
+    end
+  end
+  describe "#destroy" do
+    context "user signed in" do
+      let(:user) { create(:user) }
+      let(:another_user) { create(:user) }
+      let(:album) { create(:album, user: user) }
+
+      before do
+        login_user_with_token user
+      end
+
+      it "destroy album" do
+        delete :destroy, params: { id: album.id }, format: :json
+
+        expect(Album.any?).to eq(false)
+      end
+      it "can't destroy another's album" do
+        album.update_attributes(user: another_user)
+
+        expect do
+          delete :destroy, params: { id: album.id }, format: :json
+        end.to raise_error(CanCan::AccessDenied)
+      end
+
+
+    end
+    context "user not signed in" do
+      let(:album) { create(:album) }
+
+      it "need authorization" do
+        delete :destroy, params: { id: album.id }, format: :json
 
         is_expected.to respond_with(401)
       end
